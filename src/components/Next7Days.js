6dcce50e496015e3d3998de92f01d1b7c01ffb0c
Adinath Sangale <87353689+Xsvampire3@users.noreply.github.com>
2023-03-19 22:11:36 +0530

    Update Next7Days.js

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -2,13 +2,14 @@ import React from "react";
 import ListRender from "./ListRender";
 
 const Next7Days = (props) => {
-  const today = new Date();
-  const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000); // add 7 days to the current date
+  const date = new Date();
+  console.log(date);
+  const nextWeek = new Date(date.getTime() + 7 * 24 * 60 * 60 * 1000); // add 7 days to the current date
 
   const nseven = props.list.filter((itm) => {
     const d = new Date(itm.date);
     console.log(d,"ddddd");
-    return d <= nextWeek && d >= today;
+    return d <= nextWeek && d >= date;
   });
   console.log(nseven)
 
