c1a3ee1ad18dbf0367405dfa4cfa0b7cc50a06ec
Adinath Sangale <87353689+Xsvampire3@users.noreply.github.com>
2023-03-19 18:04:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import SideNav from './SideNav';
 import MainSection from './MainSection';
 import Header from './Header';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css';
-const App = () => {
 
+const App = () => {
+  const [activeComponent, setActiveComponent] = useState('inbox');
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+    setActiveComponent('inbox');
+  };
 
   return (
     <div id="main">
-    <Header />
+      <Header />
       <div className='row'>
         <div className='col-md-2'>
-          <SideNav change={}/>
+          <SideNav change={setActiveComponent} active={activeComponent} />
         </div>
         <div className='col-md-10'>
-          <MainSection active = {}/>
+          <MainSection
+            active={activeComponent}
+            tasks={tasks}
+            onAddTask={handleAddTask}
+          />
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
