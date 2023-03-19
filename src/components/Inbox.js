8f4961e11757f25cd7eba7a9971b7fb99a804b1a
Adinath Sangale <87353689+Xsvampire3@users.noreply.github.com>
2023-03-19 18:06:31 +0530

    Update Inbox.js

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,29 +1,55 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef } from 'react';
 
 const Inbox = (props) => {
- 
+  const [newTask, setNewTask] = useState(false);
+  const titleRef = useRef();
+  const dateRef = useRef();
+
+  const handleAddNewClick = () => {
+    setNewTask(true);
+  };
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    const newTitle = titleRef.current.value;
+    const newDate = dateRef.current.value;
+    const newTask = {
+      title: newTitle,
+      date: newDate
+    };
+    props.onAddTask(newTask);
+    setNewTask(false);
+    titleRef.current.value = '';
+    dateRef.current.value = '';
+  };
+
+  const handleCancelClick = () => {
+    setNewTask(false);
+    titleRef.current.value = '';
+    dateRef.current.value = '';
+  };
 
   return (
     <div>
       <h3>Inbox</h3>
       {!newTask && (
-        <button className="new" onClick={} id='add-new'>
+        <button className="new" onClick={handleAddNewClick} id='add-new'>
           +Add New
         </button>
       )}
       {newTask && (
         <form className="newtask-box">
-          <input type="text" id="title" ref={}></input>
+          <input type="text" id="title" ref={titleRef}></input>
           <div className="buttons">
-            <button className="new" id="add-list" onClick={}>
+            <button className="new" id="add-list" onClick={handleAddTask}>
               Add Task
             </button>
-            <button className="new" onClick={}>
+            <button className="new" onClick={handleCancelClick}>
               Cancel
             </button>
             <input
               type="date"
-              ref={}
+              ref={dateRef}
               defaultValue="2022-09-27"
               id="date"
             ></input>
@@ -31,11 +57,11 @@ const Inbox = (props) => {
         </form>
       )}
       <div id="inbox">
-        {props.list.map((list) => {
+        {props.list.map((task) => {
           return (
-            <div className="box" key={}>
+            <div className="box" key={task.title}>
               <div className="task">
-                {} ({})
+                {task.title} ({task.date})
               </div>
             </div>
           );
