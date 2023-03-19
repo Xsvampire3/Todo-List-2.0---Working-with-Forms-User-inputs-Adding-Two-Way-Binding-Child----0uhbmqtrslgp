66739ce55943667852aa9a1db29bd93a2bb98cb8
Adinath Sangale <87353689+Xsvampire3@users.noreply.github.com>
2023-03-19 18:11:16 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -3,10 +3,16 @@ import ListRender from "./ListRender";
 
 const Today = (props) => {
   const date = new Date();
-  
+  const today = date.toLocaleDateString();
+
+  // Filter list to show only tasks with today's date
+  const filteredList = props.list.filter(
+    (task) => task.date.toLocaleDateString() === today
+  );
+
   return (
     <div id="today-list">
-      <ListRender list={} />
+      <ListRender list={filteredList} />
     </div>
   );
 };
